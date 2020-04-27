1b49eca21ec8f5b3aa5f7d31bd3fcbc75644abb7
rmlzy <rmlzy@outlook.com>
2020-04-27 11:32:17 +0800

    feat: add Loading Component

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { LoadingOutlined } from "@ant-design/icons";
 
 export default class Loading extends React.Component {
   render() {
-    return <div>Loading</div>;
+    return (
+      <div style={{ display: "flex", width: "100%", height: "500px", alignItems: "center", justifyContent: "center" }}>
+        <LoadingOutlined style={{ fontSize: 20 }} />
+      </div>
+    );
   }
 }
